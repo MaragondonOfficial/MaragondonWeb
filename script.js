fcc002fb87c6063e7d0c4651d0bf6093c8924143
MaragondonOfficial <maragondon2025@gmail.com>
2025-07-05 12:47:58 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Optional JavaScript - for now it's just a placeholder
+console.log("Empire Flippers Clone Loaded!");
+
+function toggleDrawer() {
+  const drawer = document.getElementById("myDrawer");
+  if (drawer.style.width === "250px") {
+    drawer.style.width = "0";
+  } else {
+    drawer.style.width = "250px";
+  }
+}
