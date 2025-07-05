1165706c10001b9068f01ca164829a576d9c1a45
MaragondonOfficial <maragondon2025@gmail.com>
2025-07-05 14:36:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 console.log("Empire Flippers Clone Loaded!");
 
 if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
-  alert("Please view this site in desktop mode for the best experience.");
+  document.body.innerHTML = `
+    <div style="
+      background: white;
+      color: black;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      padding: 20px;
+      font-size: 1.5rem;
+      font-family: Arial, sans-serif;">
+      📵 This site is best viewed on a desktop.<br>
+      Please switch to desktop mode.
+    </div>
+  `;
 }
-
-
-
