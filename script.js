45f6f4ff10e2118a7b25f266efac5c774b696848
MaragondonOfficial <maragondon2025@gmail.com>
2025-07-05 14:31:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Optional JavaScript - for now it's just a placeholder
 console.log("Empire Flippers Clone Loaded!");
 
-function toggleDrawer() {
-  const drawer = document.getElementById("myDrawer");
-  if (drawer.style.width === "250px") {
-    drawer.style.width = "0";
-  } else {
-    drawer.style.width = "250px";
-  }
+if (/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+  alert("Please view this site in desktop mode for the best experience.");
 }
+
+
+
